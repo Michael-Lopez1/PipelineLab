2731033032fe0679ae27b84acf3b26968c7ea61b
Michael Lopez <Lopez4@sheridancollege.ca>
2025-11-18 11:25:59 -0500

    Update message to Michael

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello, Dockerized Node.js App!');
+  res.send('Michael');
 });
 
 app.listen(PORT, () => {
